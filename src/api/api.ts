79295daa14f12f36b102978f73c6cddd445cb44e
Dmitry Rybak <46091872+Carpadiem@users.noreply.github.com>
2024-04-01 02:54:26 +0300

    change api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,5 +2,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://185.250.44.55:3001/api'
+  baseURL: 'https://185.250.44.55:3001/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
